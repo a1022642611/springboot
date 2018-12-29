515dad319c140ae8ac091356e4541dc706a5596a
houlei <1022642611@qq.com>
2018-12-29 16:08:21 +0800

    [*#B1002# 修改了一个bug]

--- src/main/java/com/atguigu/springboot/controller/houleiController.java
+++ src/main/java/com/atguigu/springboot/controller/houleiController.java
@@ -3,5 +3,7 @@ package com.atguigu.springboot.controller;
 public class houleiController {
     public static void main(String[] args) {
         System.out.println("修改文件提交了");
+        System.out.println("我新建了本地分支");
+        System.out.println("我写一行代码");
     }
 }
